3922287660b58ff9c83a77061d4082c2732067c6
jr-pedersen <129087875+jr-pedersen@users.noreply.github.com>
2023-07-09 22:25:42 -0400

    Add files via upload

--- converter.py
+++ converter.py
@@ -1,8 +1,10 @@
 import csv
-csv_file_path = "/Users/*YOURPATH*/desktop/gatewayDistances.csv"
-o_file = "Glenn"  # ASSET NAME e.g. "TDRSX" OR " Goddard"
+csv_file_path = "/Users/jakepedersen/desktop/gatewayDistances.csv"
+o_file = "Guam"  # ASSET NAME e.g. "TDRSX" OR "Goddard"
 
 # converter fcn
+
+
 def convertDistance(distance):
     return distance/300000
 
@@ -13,13 +15,14 @@ try:
         next(reader)  # header
         with open(o_file, "w") as output:
             output.write(f"# Name : {o_file}\n")
-            output.write("# Contents : BW, DELAY\n")
+            output.write("# Contents : DELAY\n")
             for row in reader:
                 time = row[0]
-                distance = float(row[6])
+                distance = float(row[1])
                 converted_time = convertDistance(distance)
                 output.write(f"{time}, {converted_time}\n")
-    print(f"Data imported successfully. The new file '{o_file}' has been created with converted vals")
+    print(
+        f"Data imported successfully. The new file '{o_file}' has been created with converted vals")
 
 except Exception as x:
     print("F", str(x))
